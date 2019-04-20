33a580b9389e0d9df5c80fde0cdb13496b231b9a
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-20 21:28:07 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+array = [1, 2, 3]
+
+def my_each(array)
+  if block_given?
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+    array
+  else
+    puts "Hey! Ny Block was given!"
+  end
 end
